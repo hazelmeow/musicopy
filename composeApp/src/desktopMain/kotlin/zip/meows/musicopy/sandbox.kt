ca79747c424299a5df91c10c7137e65b72372edc
hazel <hazel@meows.zip>
2025-07-04 14:22:19 -0400

    sandbox

--- composeApp/src/desktopMain/kotlin/zip/meows/musicopy/sandbox.kt
+++ composeApp/src/desktopMain/kotlin/zip/meows/musicopy/sandbox.kt
@@ -2,22 +2,17 @@ package zip.meows.musicopy
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.composeunstyled.Text
-import zip.meows.musicopy.ui.DesktopApp
-import java.awt.Dimension
-
-
-const val WINDOW_WIDTH = 800
-const val WINDOW_HEIGHT = 600
+import zip.meows.musicopy.ui.screens.ConnectManuallyScreen
 
 fun main() = application {
     val state = rememberWindowState(
@@ -25,19 +20,34 @@ fun main() = application {
     )
 
     Window(
-        title = "Musicopy",
+        title = "Musicopy [Sandbox]",
         onCloseRequest = ::exitApplication,
         state = state
     ) {
-//        window.minimumSize = Dimension(800, 600)
-
         val platformContext = PlatformContext(mainWindow = window)
 
-        DesktopApp(platformContext)
+        Sandbox(platformContext)
 
         // TODO
         Box(modifier = Modifier.offset(x = 8.dp, y = 8.dp)) {
             Text("window: ${LocalWindowInfo.current.containerSize}")
         }
     }
+}
+
+@Composable
+private fun Sandbox(
+    platformContext: PlatformContext,
+) {
+    MaterialTheme {
+        SandboxContent()
+    }
+}
+
+@Composable
+private fun SandboxContent() {
+    ConnectManuallyScreen(
+        onSubmit = {},
+        onCancel = {},
+    )
 }
