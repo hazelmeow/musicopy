cfdb3c030f67989a44269180a06d49f3e06d84cc
hazel <hazel@meows.zip>
2025-07-04 13:11:29 -0400

    client actor

--- crates/musicopy/src/node.rs
+++ crates/musicopy/src/node.rs
@@ -1,3 +1,13 @@
+//! Networking.
+//!
+//! A `Node` is an Iroh node that can perform the client or server end of the protocol.
+//!
+//! A `Client` is a struct representing an outgoing connection to a server.
+//! Clients request files, primarily used in the mobile app.
+//!
+//! A `Server` is a struct representing an incoming connection from a client.
+//! Servers send files, primarily used in the desktop app.
+
 use crate::database::Database;
 use futures::{SinkExt, StreamExt, TryStreamExt};
 use iroh::{
@@ -19,8 +29,19 @@ use tokio_util::{
     codec::{FramedRead, FramedWrite, LengthDelimitedCodec},
 };
 
+/// Model of an incoming connection.
 #[derive(Debug, uniffi::Record)]
 pub struct ConnectionModel {
+    pub name: String,
+    pub node_id: String,
+    pub connected_at: u64,
+    pub connection_type: String,
+    pub latency_ms: Option<u64>,
+}
+
+/// Model of an outgoing connection.
+#[derive(Debug, uniffi::Record)]
+pub struct ClientModel {
     pub name: String,
     pub node_id: String,
     pub connected_at: u64,
@@ -45,6 +66,9 @@ pub struct NodeModel {
 
     active_connections: Vec<ConnectionModel>,
     pending_connections: Vec<ConnectionModel>,
+
+    active_clients: Vec<ClientModel>,
+    pending_clients: Vec<ClientModel>,
 }
 
 #[derive(Debug)]
@@ -68,12 +92,17 @@ pub struct Node {
     db: Arc<Mutex<Database>>,
     router: Router,
     protocol: Protocol,
+
+    client_handle_tx: mpsc::UnboundedSender<(NodeId, ClientHandle)>,
+
     peers: Mutex<HashMap<NodeId, PeerHandle>>,
+    clients: Mutex<HashMap<NodeId, ClientHandle>>,
 }
 
 #[derive(Debug)]
 pub struct NodeRunToken {
     peer_rx: mpsc::UnboundedReceiver<(NodeId, PeerHandle)>,
+    client_handle_rx: mpsc::UnboundedReceiver<(NodeId, ClientHandle)>,
 }
 
 impl Node {
@@ -82,6 +111,7 @@ impl Node {
         db: Arc<Mutex<Database>>,
     ) -> anyhow::Result<(Arc<Self>, NodeRunToken)> {
         let (peer_tx, peer_rx) = mpsc::unbounded_channel();
+        let (client_handle_tx, client_handle_rx) = mpsc::unbounded_channel();
 
         let endpoint = Endpoint::builder()
             .secret_key(secret_key)
@@ -98,10 +128,17 @@ impl Node {
             db,
             router,
             protocol,
+
+            client_handle_tx,
+
             peers: Mutex::new(HashMap::new()),
+            clients: Mutex::new(HashMap::new()),
         });
 
-        let node_run = NodeRunToken { peer_rx };
+        let node_run = NodeRunToken {
+            peer_rx,
+            client_handle_rx,
+        };
 
         Ok((node, node_run))
     }
@@ -111,7 +148,10 @@ impl Node {
         mut rx: mpsc::UnboundedReceiver<NodeCommand>,
         run_token: NodeRunToken,
     ) -> anyhow::Result<()> {
-        let NodeRunToken { mut peer_rx } = run_token;
+        let NodeRunToken {
+            mut peer_rx,
+            mut client_handle_rx,
+        } = run_token;
 
         loop {
             tokio::select! {
@@ -150,6 +190,11 @@ impl Node {
                     let mut peers = self.peers.lock().unwrap();
                     peers.insert(peer_id, peer_handle);
                 }
+
+                Some((client_id, client_handle)) = client_handle_rx.recv() => {
+                    let mut clients = self.clients.lock().unwrap();
+                    clients.insert(client_id, client_handle);
+                }
             }
             tokio::time::sleep(Duration::from_secs(1)).await;
         }
@@ -160,96 +205,22 @@ impl Node {
     }
 
     async fn connect(self: &Arc<Self>, addr: NodeAddr) -> anyhow::Result<()> {
-        // connect
-        let conn = self.router.endpoint().connect(addr, Protocol::ALPN).await?;
+        // connect before spawning the task, so we can return an error immediately
+        let connection = self.router.endpoint().connect(addr, Protocol::ALPN).await?;
 
-        // open a bidirectional QUIC stream
-        let (send, recv) = conn.open_bi().await?;
+        let node_id = connection.remote_node_id()?;
+        log::info!("opened connection to {node_id}");
 
-        // wrap in framed codecs
-        let mut send = FramedWrite::new(send, LengthDelimitedCodec::new()).with_flat_map(
-            |message: ClientMessage| {
-                let buf: Vec<u8> =
-                    postcard::to_stdvec(&message).expect("failed to serialize message");
-                futures::stream::once(futures::future::ready(Ok(Bytes::from(buf))))
-            },
-        );
-        let mut recv = FramedRead::new(recv, LengthDelimitedCodec::new())
-            .map_err(|e| anyhow::anyhow!("failed to read from connection: {e:?}"))
-            .map(|res| {
-                res.and_then(|bytes| {
-                    postcard::from_bytes::<ServerMessage>(&bytes)
-                        .map_err(|e| anyhow::anyhow!("failed to deserialize message: {e:?}"))
-                })
-            });
+        let client_handle_tx = self.client_handle_tx.clone();
+        tokio::spawn(async move {
+            let client = Client::new(connection, client_handle_tx);
 
-        // send client Identify
-        // TODO: real name
-        send.send(ClientMessage::Identify("client".to_string()))
-            .await
-            .expect("failed to send Identify message");
-
-        // wait for server Identify
-        let Some(Ok(message)) = recv.next().await else {
-            log::error!("failed to receive Identify message");
-            return Ok(());
-        };
-        match message {
-            ServerMessage::Identify(name) => {
-                // TODO: store
-                log::info!("server identified as {name}");
+            if let Err(e) = client.run().await {
+                log::error!("error during client.run(): {e}");
             }
-            _ => {
-                log::error!("unexpected message, expected Identify: {message:?}");
-                return Ok(());
-            }
-        }
 
-        // wait for server Accepted
-        let Some(Ok(message)) = recv.next().await else {
-            log::error!("failed to receive Accepted message");
-            return Ok(());
-        };
-        match message {
-            ServerMessage::Accepted => {
-                log::info!("server accepted the connection");
-            }
-            _ => {
-                log::error!("unexpected message, expected Accepted: {message:?}");
-                return Ok(());
-            }
-        }
-
-        // wait for server Index
-        let Some(Ok(message)) = recv.next().await else {
-            log::error!("failed to receive Index message");
-            return Ok(());
-        };
-        let index = match message {
-            ServerMessage::Index(index) => {
-                log::info!("received index with {} items", index.len());
-                index
-            }
-            _ => {
-                log::error!("unexpected message, expected Index: {message:?}");
-                return Ok(());
-            }
-        };
-
-        // TODO: open streams and such
-
-        loop {
-            tokio::select! {
-                Some(Ok(message)) = recv.next() => {
-                    log::debug!("received message: {message:?}");
-                }
-
-                _ = conn.closed() => {
-                    log::info!("connection closed");
-                    break;
-                }
-            }
-        }
+            // TODO: remove handle from hashmap
+        });
 
         Ok(())
     }
@@ -301,6 +272,40 @@ impl Node {
             (active_connections, pending_connections)
         };
 
+        let (active_clients, pending_clients) = {
+            let clients = self.clients.lock().unwrap();
+            let (mut active_clients, mut pending_clients) = clients
+                .iter()
+                .map(|(node_id, client_handle)| {
+                    let accepted = client_handle
+                        .accepted
+                        .load(std::sync::atomic::Ordering::Relaxed);
+
+                    let remote_info = self.router.endpoint().remote_info(*node_id);
+                    let connection_type = remote_info
+                        .as_ref()
+                        .map(|info| info.conn_type.to_string())
+                        .unwrap_or_else(|| "unknown".to_string());
+                    let latency_ms = remote_info
+                        .and_then(|info| info.latency)
+                        .map(|latency| latency.as_millis() as u64);
+
+                    let model = ClientModel {
+                        name: "unknown".to_string(), // TODO: get real name
+                        node_id: node_id.to_string(),
+                        connected_at: client_handle.connected_at,
+                        connection_type,
+                        latency_ms,
+                    };
+
+                    if accepted { Ok(model) } else { Err(model) }
+                })
+                .partition_result::<Vec<_>, Vec<_>, _, _>();
+            active_clients.sort_by_key(|c| c.connected_at);
+            pending_clients.sort_by_key(|c| c.connected_at);
+            (active_clients, pending_clients)
+        };
+
         NodeModel {
             node_id: self.router.endpoint().node_id().to_string(),
             home_relay,
@@ -316,6 +321,9 @@ impl Node {
 
             active_connections,
             pending_connections,
+
+            active_clients,
+            pending_clients,
         }
     }
 }
@@ -339,32 +347,15 @@ impl ProtocolHandler for Protocol {
         let db = self.db.clone();
         let handle_tx = self.peer_tx.clone();
         Box::pin(async move {
-            // We can get the remote's node id from the connection.
             let node_id = connection.remote_node_id()?;
-            println!("accepted connection from {node_id}");
+            log::info!("accepted connection from {node_id}");
 
-            let mut peer = Peer::new(db, connection, handle_tx);
+            let peer = Peer::new(db, connection, handle_tx);
             peer.run().await?;
-
-            peer.connection.closed().await;
 
             Ok(())
         })
     }
-}
-
-#[derive(Debug)]
-enum PeerCommand {
-    Accept,
-
-    Close,
-}
-
-#[derive(Debug, Clone)]
-struct PeerHandle {
-    connected_at: u64,
-    accepted: Arc<AtomicBool>,
-    tx: mpsc::UnboundedSender<PeerCommand>,
 }
 
 /// A message sent by the client end of a connection.
@@ -389,14 +380,26 @@ enum ServerMessage {
     Index(Vec<IndexItem>),
 }
 
+#[derive(Debug)]
+enum PeerCommand {
+    Accept,
+
+    Close,
+}
+
+#[derive(Debug, Clone)]
+struct PeerHandle {
+    connected_at: u64,
+    accepted: Arc<AtomicBool>,
+    tx: mpsc::UnboundedSender<PeerCommand>,
+}
+
 struct Peer {
     db: Arc<Mutex<Database>>,
     connection: Connection,
     handle_tx: mpsc::UnboundedSender<(NodeId, PeerHandle)>,
     connected_at: u64,
     accepted: Arc<AtomicBool>,
-    tx: mpsc::UnboundedSender<PeerCommand>,
-    rx: mpsc::UnboundedReceiver<PeerCommand>,
 }
 
 impl Peer {
@@ -405,8 +408,6 @@ impl Peer {
         connection: Connection,
         handle_tx: mpsc::UnboundedSender<(NodeId, PeerHandle)>,
     ) -> Self {
-        let (tx, rx) = mpsc::unbounded_channel();
-
         Self {
             db,
             connection,
@@ -416,13 +417,13 @@ impl Peer {
                 .unwrap_or_default()
                 .as_secs(),
             accepted: Arc::new(AtomicBool::new(false)),
-            tx,
-            rx,
         }
     }
 
-    async fn run(&mut self) -> anyhow::Result<()> {
+    async fn run(self) -> anyhow::Result<()> {
         let remote_node_id = self.connection.remote_node_id()?;
+
+        let (tx, mut rx) = mpsc::unbounded_channel();
 
         // accept bidirectional control stream
         let (send, recv) = self.connection.accept_bi().await?;
@@ -466,22 +467,22 @@ impl Peer {
             .await
             .expect("failed to send Identify message");
 
-        // send handle to Node
+        // handshake finished, send handle to Node
         self.handle_tx
             .send((
                 remote_node_id,
                 PeerHandle {
                     connected_at: self.connected_at,
                     accepted: self.accepted.clone(),
-                    tx: self.tx.clone(),
+                    tx: tx.clone(),
                 },
             ))
             .expect("failed to send peer handle");
 
-        // waiting loop
+        // waiting loop, wait for user to accept or deny the connection
         loop {
             tokio::select! {
-                Some(command) = self.rx.recv() => {
+                Some(command) = rx.recv() => {
                     match command {
                         PeerCommand::Accept => {
                             // continue to next state
@@ -495,10 +496,8 @@ impl Peer {
                 }
 
                 Some(Ok(message)) = recv.next() => {
-                    log::debug!("unexpected message (waiting): {message:?}");
+                    log::debug!("unexpected message (waiting  for Accepted): {message:?}");
                 }
-
-                else => break,
             }
         }
 
@@ -521,7 +520,7 @@ impl Peer {
         // main loop
         loop {
             tokio::select! {
-                Some(command) = self.rx.recv() => {
+                Some(command) = rx.recv() => {
                     match command {
                         PeerCommand::Accept => {
                             log::warn!("unexpected Accept command in main loop");
@@ -558,5 +557,171 @@ impl Peer {
                 path: file.path,
             })
             .collect())
+    }
+}
+
+#[derive(Debug)]
+enum ClientCommand {
+    Close,
+}
+
+#[derive(Debug, Clone)]
+struct ClientHandle {
+    connected_at: u64,
+    accepted: Arc<AtomicBool>,
+    tx: mpsc::UnboundedSender<ClientCommand>,
+}
+
+struct Client {
+    connection: Connection,
+    handle_tx: mpsc::UnboundedSender<(NodeId, ClientHandle)>,
+    connected_at: u64,
+    accepted: Arc<AtomicBool>,
+}
+
+impl Client {
+    fn new(
+        connection: Connection,
+        handle_tx: mpsc::UnboundedSender<(NodeId, ClientHandle)>,
+    ) -> Self {
+        Self {
+            connection,
+            handle_tx,
+            connected_at: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap_or_default()
+                .as_secs(),
+            accepted: Arc::new(AtomicBool::new(false)),
+        }
+    }
+
+    async fn run(self) -> anyhow::Result<()> {
+        let remote_node_id = self.connection.remote_node_id()?;
+
+        let (tx, mut rx) = mpsc::unbounded_channel();
+
+        // open a bidirectional QUIC stream
+        let (send, recv) = self.connection.open_bi().await?;
+
+        // wrap in framed codecs
+        let mut send = FramedWrite::new(send, LengthDelimitedCodec::new()).with_flat_map(
+            |message: ClientMessage| {
+                let buf: Vec<u8> =
+                    postcard::to_stdvec(&message).expect("failed to serialize message");
+                futures::stream::once(futures::future::ready(Ok(Bytes::from(buf))))
+            },
+        );
+        let mut recv = FramedRead::new(recv, LengthDelimitedCodec::new())
+            .map_err(|e| anyhow::anyhow!("failed to read from connection: {e:?}"))
+            .map(|res| {
+                res.and_then(|bytes| {
+                    postcard::from_bytes::<ServerMessage>(&bytes)
+                        .map_err(|e| anyhow::anyhow!("failed to deserialize message: {e:?}"))
+                })
+            });
+
+        // send client Identify
+        // TODO: real name
+        send.send(ClientMessage::Identify("client".to_string()))
+            .await
+            .expect("failed to send Identify message");
+
+        // wait for server Identify
+        // TODO: also wait for commands
+        let Some(Ok(message)) = recv.next().await else {
+            log::error!("failed to receive Identify message");
+            return Ok(());
+        };
+        match message {
+            ServerMessage::Identify(name) => {
+                // TODO: store
+                log::info!("server identified as {name}");
+            }
+            _ => {
+                log::error!("unexpected message, expected Identify: {message:?}");
+                return Ok(());
+            }
+        }
+
+        // handshake finished, send handle to Node
+        self.handle_tx
+            .send((
+                remote_node_id,
+                ClientHandle {
+                    connected_at: self.connected_at,
+                    accepted: self.accepted.clone(),
+                    tx,
+                },
+            ))
+            .expect("failed to send server handle");
+
+        // waiting loop, wait for server Accepted
+        loop {
+            tokio::select! {
+                Some(command) = rx.recv() => {
+                    match command {
+                        ClientCommand::Close => {
+                            return Ok(());
+                        }
+                    }
+                }
+
+                Some(Ok(message)) = recv.next() => {
+                    match message {
+                        ServerMessage::Accepted => {
+                            log::info!("server accepted the connection");
+
+                            // continue to next state
+                            break;
+                        }
+                        _ => {
+                            log::debug!("unexpected message (waiting for Accepted): {message:?}");
+                        }
+                    }
+                }
+            }
+        }
+
+        // mark as accepted
+        self.accepted
+            .store(true, std::sync::atomic::Ordering::Relaxed);
+
+        // main loop
+        loop {
+            tokio::select! {
+                Some(command) = rx.recv() => {
+                    match command {
+                        ClientCommand::Close => {
+                            self.connection.close(0u32.into(), b"close");
+                            break;
+                        }
+                    }
+                }
+
+                Some(Ok(message)) = recv.next() => {
+                    log::debug!("received message: {message:?}");
+
+                    match message {
+                        ServerMessage::Index(index) => {
+                            log::info!("received index with {} items", index.len());
+                            // TOOD: do something
+                        }
+
+                        _ => {
+                            log::debug!("unexpected message in main loop: {message:?}");
+                        }
+                    }
+                }
+
+                _ = self.connection.closed() => {
+                    log::info!("connection closed");
+                    break;
+                }
+            }
+        }
+
+        self.connection.closed().await;
+
+        Ok(())
     }
 }
